a85d75877869080aed1d435c2fe8e0e6a7b468ec
aswinashrestha <aswinashrestha@gmail.com>
2024-06-06 17:54:03 +0545

    initial commit :rocket:

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import Player from "./component/Player.jsx";
-function App() {
+import { useState } from "react";
 
-  return <main>
-    <div id = "game-container">
-      <ol id = "players">
-        <Player name ="Player 1" symbol="X" />
-        <Player name ="Player 2" symbol="O" />
-      </ol>
-     GAME BOARD
-    </div>
-    </main>;
+import Player from "./component/Player.jsx";
+import GameBoard from "./component/GameBoard.jsx";
+import Log from "./component/Log.jsx";
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
+      </div>
+      <Log />
+    </main>
+  );
 }
 
-export default App
+export default App;
